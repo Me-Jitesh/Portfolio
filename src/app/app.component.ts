0e9cda3e63e84d1b0acf8e85c81ac2543ec5e23e
Jitesh Singh <jiteshs101@gmail.com>
2024-09-17 23:35:03 +0530

    Initial layout designed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DeploymentsComponent } from "./deployments/deployments.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { HeroSectionComponent } from "./hero-section/hero-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DeploymentsComponent, SkillsComponent, HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
